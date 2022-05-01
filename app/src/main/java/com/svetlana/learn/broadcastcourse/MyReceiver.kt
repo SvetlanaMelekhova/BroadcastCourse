cf896ed4ad8e18100e049504d403c07cf1c8a9ce
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-01 21:03:28 +0300

    lesson1

---
+++ app/src/main/java/com/svetlana/learn/broadcastcourse/MyReceiver.kt
@@ -0,0 +1,17 @@
+package com.svetlana.learn.broadcastcourse
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action){
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                Toast.makeText(context, "Airplane has changed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
